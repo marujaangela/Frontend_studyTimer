a72c08349e25ca3959ad802c766075bf5036be1f
marujaangela <s0591654@htw-berlin.de>
2025-06-30 15:52:53 +0200

    Update Service Port festgelegt

--- Frontend_studyTimer/vite.config.ts
+++ Frontend_studyTimer/vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
     vue(),
     vueDevTools()
   ],
+  server: {
+    port: 5175 // ⬅️ Hier legst du den Port fest
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
